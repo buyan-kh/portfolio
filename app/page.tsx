f4fdd62028a16113f8085198b47234d62b3fb86e
Buyantogtokh Khurelbaatar <vicekurel@gmail.com>
2025-02-20 20:55:31 -0800

    landing screen done

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Divider from "@/components/divider";
 import Start from "@/components/start";
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Start />
+      <Divider />
     </main>
   );
 }
--- components/start.tsx
+++ components/start.tsx
@@ -3,6 +3,9 @@ import { MotionConfig } from "framer-motion";
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsGithub, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
 
 export default function Start() {
   return (
@@ -54,6 +57,55 @@ export default function Start() {
         intelligent solutions. My current focus is
         <span className="underline"> Machine Learning, AI. </span>
       </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full
+          outline-none focus:outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105
+          transition"
+        >
+          Contact me here
+          <BsArrowRight className="opacity-70 group-hover:translate-x-2 transition" />
+        </Link>
+
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none
+          focus:scale-110 hover:scale-110 active:scale-10 transition cursor-pointer border border-black/10"
+          href="/Resume.pdf"
+          download
+        >
+          My Resume{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        <a
+          className="bg-white p-4 flex text-gray-700 items-center gap-2 rounded-full
+          focus:scale-[1.4] hover:scale-[1.4] hover:text-blue-600 0 active:scale-10 transition cursor-pointer
+          border border-black/10"
+          href="https://www.linkedin.com/in/khurel/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 flex text-gray-700 items-center gap-2 rounded-full
+          focus:scale-[1.4] hover:scale-[1.4] hover:text-orange-400 active:scale-10 transition cursor-pointer
+          border border-black/10"
+          href="https://github.com/buyan-kh"
+          target="_blank "
+        >
+          <BsGithub />
+        </a>
+      </motion.div>
     </section>
   );
 }
